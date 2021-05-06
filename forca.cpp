d665294221eb92ccc701dc3c55ca8d358a21bf91
BAVARESCO-PC\Bavaresco <maopdtec@gmail.com>
2021-05-06 13:26:16 -0300

    Curso C II Alura jogo da forca

--- forca.cpp
+++ forca.cpp
@@ -1,54 +1,161 @@
 #include<stdio.h>
 #include<string.h>
 #include<locale.h>
-int main()
-{
-	setlocale(LC_ALL,"portuguese");
-	char palavra[20];
-	sprintf(palavra,"MELANCIA");
-	int acertou=0;
-	int enforcou = 0;
-//	printf("%d | %d",acertou,!acertou);
+#include"forca.h"
+#include<time.h>
+#include<stdlib.h>
+
+	char palavra[TAMANHO_PALAVRA];
 	char chutes[26];
-	int tentativas = 0;
-	do{
-		
+	int chutesDados = 0;
+	
+void abertura()
+{
+	printf("/****************/\n");
+	printf("/ Jogo de Forca */\n");
+	printf("/****************/\n\n");
+}
+void chuta()
+{
+		char chute;
+		scanf(" %c",&chute);
+		chutes[(chutesDados)]=chute;
+		(chutesDados)++;
+	
+}
+
+	void desenhaForca()
+	{
 		for(int i=0;i<strlen(palavra);i++)
-		{
-			int achou=0;
-			for(int j=0;j<tentativas;j++)
 			{
-				if(chutes[j]==palavra[i])
+				int achou=jaChutou(palavra[i]);
+					if(achou)
+					{
+						printf(" %c ",palavra[i]);
+					}
+					else
+					{
+						printf("_ ");	
+					}
+		 	
+			}
+			printf("\n");
+	}
+
+void adicionaPalavra()
+{
+	char quer;
+	printf("\nVocê deseja adicionar uma nova palavra no jogo?(S/N)\n");
+	scanf(" %c", &quer);
+	if(quer=='S')
+	{
+		char novaPalavra[TAMANHO_PALAVRA];
+		printf("Qual a nova palavra? ");
+		scanf("%s",novaPalavra);
+		FILE * f;
+		f= fopen("palavra.txt","r+");
+		if(f==0){
+		printf("Banco de dados não disponível!\n");
+		exit(1);
+	}
+		
+		
+		int qtd;
+		fscanf(f,"%d",&qtd);
+		qtd++;
+		fseek(f,0,SEEK_SET);
+		fprintf(f,"%d",qtd);
+		fseek(f,0,SEEK_END);
+		fprintf(f,"\n%s",novaPalavra);
+		fclose(f);
+	}
+}
+
+	void escolhePalavra()
+	{
+	FILE * f;
+	f=	fopen("palavra.txt","r");
+	if(f==0){
+		printf("Banco de dados não disponível!\n");
+		exit(1);
+	}
+	int qntPalavras;
+	fscanf(f," %d ",&qntPalavras);
+	srand(time(0));
+	int randomico=rand()% qntPalavras;
+	for(int i=0;i<randomico;i++)
+	{
+		fscanf(f,"%s",palavra);
+	}
+	fclose(f);
+	}
+	
+	
+int enforcou()
+{
+	int erros=0;
+	for(int i=0;i<chutesDados;i++)
+	{
+		int existe=0;
+		for(int j=0;j<strlen(palavra);j++)
+		{
+			if(chutes[i]==palavra[j])
+			{
+				existe=1;
+				break;
+			}
+		}
+		if(!existe) erros++;
+		 
+	}
+	return erros>=5;
+}
+	
+int acertou()
+{
+	for(int i=0;i<strlen(palavra);i++)
+	{
+		if(!jaChutou(palavra[i]))
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+int jaChutou(char letra)
+{
+	int achou=0;
+			for(int j=0;j<chutesDados;j++)
+			{
+				if(chutes[j]==letra)
 				{
 					achou=1;
 					break;
 				}
 			}
-			if(achou)
-			{
-				printf("%c",palavra[i]);
-			}
-			else
-			{
-				printf("_ ");	
-			}
-				
-			
-			
-			
-			
+			return achou;
+}	
+int main()
+{
+	setlocale(LC_ALL,"portuguese");
+
+
+//	int acertou=0;
+//	int enforcou = 0;
+//	printf("%d | %d",acertou,!acertou);
+	escolhePalavra();
+		abertura();
+	do{
+	
+		desenhaForca();
 		
-		}
-		printf("\n");
 		
-		
-		char chute;
-		scanf(" %c",&chute);
-		chutes[tentativas]=chute;
-		tentativas++;
+		chuta();
+// 	chutesDados++;
         
         
         
         
-	}while(!acertou  && !enforcou);
+	}while(!acertou()  && !enforcou());
+	adicionaPalavra();
 }
---
+++ forca.h
@@ -0,0 +1,9 @@
+#define TAMANHO_PALAVRA 20
+void abertura();
+	void chuta();
+	void desenhaForca();
+	void escolhePalavra();
+	int enforcou();
+	int acertou();
+	int jaChutou(char letra);
+void adicionaPalavra();
